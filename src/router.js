f3a8cc947baa3505f967eb518a0acc75483f9052
CodeDad <github email address>
2018-08-22 23:08:55 -0400

    created routes

--- src/router.js
+++ src/router.js
@@ -1,9 +1,21 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {Router,
+  Route, 
+  Switch,
+}from 'react-router-dom'
+import Email from './components/Email'
+import Password from './components/Password'
+import Timezones from './components/Timezones'
 
-const Router = function Router() {
-  //Put your routes inside the BrowserRouter
-  return <BrowserRouter />;
-};
+export default () => (
+  <Router>
+    <Switch>
+      <Route exact path = "/email" component={Email}/>
+      <Route exact path = "/password" component={Password}/>
+      <Route exact path = '/timezones' comoponent={Timezones}/>
+      </Switch>
+  </Router>    
+)
 
-export default Router;
+
