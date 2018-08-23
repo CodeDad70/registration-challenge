60a923cc87002ea6e5520e35082e64db450403cd
CodeDad <github email address>
2018-08-22 23:26:25 -0400

    fixed Router to BrowserRouter - routes now work

---
+++ src/components/Email.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Email extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Test</h1>
+        </div>
+    )
+  }
+}
+
+export default Email 
+
--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import {Router,
+
+import {BrowserRouter,
   Route, 
   Switch,
 }from 'react-router-dom'
@@ -9,13 +9,13 @@ import Password from './components/Password'
 import Timezones from './components/Timezones'
 
 export default () => (
-  <Router>
+  <BrowserRouter>
     <Switch>
       <Route exact path = "/email" component={Email}/>
       <Route exact path = "/password" component={Password}/>
       <Route exact path = '/timezones' comoponent={Timezones}/>
       </Switch>
-  </Router>    
+  </BrowserRouter>    
 )
 
 
